d3d6536055eb9803bac8211e8945ebf6db1c4932
Ramunas <rambek@gmail.com>
2020-09-10 12:10:10 +0300

    Initial commit

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+let user = new Human('Ramunas', 88);
+console.log(user);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,13 @@
+class Human {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let user: Human = new Human('Ramunas', 88);
+
+console.log(user);
